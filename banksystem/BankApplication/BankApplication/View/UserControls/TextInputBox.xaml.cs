796becc44804ce4a0a769a7dee7a29a9f21c1c05
Erik Wöhry <woer22la@student.ju.se>
2024-04-03 21:47:25 +0200

    new initialized wpf project instead of old project

---
+++ banksystem/BankApplication/BankApplication/View/UserControls/TextInputBox.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace BankApplication.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for TextInputBox.xaml
+    /// </summary>
+    public partial class TextInputBox : UserControl
+    {
+        public TextInputBox()
+        {
+            InitializeComponent();
+        }
+
+        public string TextString
+        {
+            get { return txtInput.Text; }
+        }
+
+        private string placeholder;
+
+        public string Placeholder
+        {
+            get { return placeholder; }
+            set 
+            { 
+                placeholder = value; 
+                txtInput.Text = placeholder;
+                //maybe there is a smarter way, ex onPropertyChanged()?
+            }
+        }
+
+        private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtInput.Text))
+                tbPlaceholder.Visibility = Visibility.Visible;
+            else    
+                tbPlaceholder.Visibility = Visibility.Hidden;
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtInput.Clear();
+            txtInput.Focus();
+        }
+    }
+}
