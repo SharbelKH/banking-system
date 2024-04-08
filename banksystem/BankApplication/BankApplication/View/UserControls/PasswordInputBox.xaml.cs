23f41bfe6ef2e6c037dae33e90f8b636debcbf36
Erik Wöhry <erik.wohry@icloud.com>
2024-04-08 15:48:21 +0200

    added styling, generic usercontrols and login layout

---
+++ banksystem/BankApplication/BankApplication/View/UserControls/PasswordInputBox.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+
+namespace BankApplication.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for PasswordInputBox.xaml
+    /// </summary>
+    public partial class PasswordInputBox : UserControl
+    {
+        public PasswordInputBox()
+        {
+            InitializeComponent();
+        }
+
+        public string passwordString
+        { 
+            get { return passwordInput.Password; }
+        }
+
+        private string placeholder;
+
+        public string Placeholder
+        {
+            get { return placeholder; }
+            set 
+            { 
+               placeholder = value; 
+               passwordInput.Password = placeholder;
+            }
+        
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            passwordInput.Clear();
+            passwordInput.Focus();
+        }
+
+    }
+}
