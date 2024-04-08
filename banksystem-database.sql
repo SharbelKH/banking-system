e51ba1a8c9d98732f08f76ca53c9012c9ed4d443
Rebecca Huynh <rebeccahuynh0@gmail.com>
2024-04-08 13:43:06 +0200

    created new tables

--- banksystem-database.sql
+++ banksystem-database.sql
@@ -1,13 +1,10 @@
 create database banksystem;
 
-CREATE TABLE Customers( ID int NOT NULL, FirstName varchar(255), LastName varchar(255), PRIMARY KEY(ID));
+CREATE TABLE Agent(ID INT NOT NULL PRIMARY KEY IDENTITY, Name VARCHAR(50) NOT NULL, Pass VARCHAR(50) NOT NULL, PhoneNumber VArCHAR(50) NOT NULL, Address VARCHAR(50) NOT NULL);
 
-INSERT INTO Customers(ID, FirstName, LastName)
-VALUES	(1, 'John', 'Doe'),
-		(2, 'Jane', 'Doe'),
-		(3, 'Bob', 'Brown'),
-		(4, 'Frank', 'Stein'),
-		(5, 'Steve', 'Hardy');
+CREATE TABLE AdminTbl(AdID INT NOT NULL PRIMARY KEY, AdName VARCHAR(50) NOT NULL, AdPass VARCHAR(50) NOT NULL);
 
-SELECT *
-FROM Customers;
+CREATE TABLE Account(CNumber INT NOT NULL PRIMARY KEY IDENTITY(1000,1), CName VARCHAR(50) NOT NULL, CPhone VARCHAR(50) NOT NULL, CAddress VARCHAR(100) NOT NULL, CGen VARCHAR(10) NOT NULL, CBal INT NOT NULL);
+
+CREATE TABLE TransferTbl( TrID INT NOT NULL PRIMARY KEY IDENTITY, TrSrc INT NOT NULL, TrDest INT NOT NULL, TrAmt INT NOT NULL, TrDate DATE NOT NULL);
+
