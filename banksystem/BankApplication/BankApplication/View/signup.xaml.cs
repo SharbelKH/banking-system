a8a931e346b6cd7ac919b94c8155051b9bbd215c
Erik Wöhry <erik.wohry@icloud.com>
2024-04-11 22:35:08 +0200

    New sign up window template implemented

---
+++ banksystem/BankApplication/BankApplication/View/signup.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace BankApplication.View
+{
+    /// <summary>
+    /// Interaction logic for signup.xaml
+    /// </summary>
+    public partial class signup : Window
+    {
+        public signup()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_CreateUser_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
