ea9501186da7af963b5b7c70f89649cf898f5acf
Emil Froding <frem21jv@student.ju.se>
2024-04-23 07:45:02 +0200

    Added balance to User, wrote method for Deposit_click in MainWindow.xaml.cs

--- banksystem/BankApplication/BankApplication/MainWindow.xaml.cs
+++ banksystem/BankApplication/BankApplication/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Identity.Client;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +10,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using BankApplication.logic;
+
 
 namespace BankApplication
 {
@@ -21,8 +25,10 @@ namespace BankApplication
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        private User logged_in_user;
+        public MainWindow(string accountId)
         {
+            this.logged_in_user = new User(accountId);
             InitializeComponent();
         }
 
@@ -37,6 +43,42 @@ namespace BankApplication
         }
 
         private void Deposit_Click(object sender, RoutedEventArgs e)
+        {
+            
+            string depositAmmount = DepositAmount.Text;
+            string ConString = OurSqlConnectionString.ConString;
+            
+            // Deposit is larger than 10 000 and therefore false
+            if (!logged_in_user.Deposit(double.Parse(depositAmmount)))
+            {
+                // Maybe window saying its not possible to deposit this ammount?
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection Con = new SqlConnection(ConString))
+                {
+                    string query = "UPDATE Account SET Balance = Balance + @new_balance WHERE Id = @AccountId";
+                    using (SqlCommand command = new SqlCommand(query, Con))
+                    {
+                        command.Parameters.AddWithValue("@AccountId", logged_in_user.UserId);
+                        command.Parameters.AddWithValue("@new_balance", depositAmmount);
+
+                        Con.Open();
+                        command.ExecuteScalar();
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Some exception window maybe?
+            }
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
