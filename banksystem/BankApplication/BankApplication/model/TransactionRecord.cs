baa9c043a60d97f4ece81e38175a72ee4bec9888
SharbelKH <sharbelkhoury92@gmail.com>
2024-05-20 13:04:57 +0200

    added TransactionRecord

---
+++ banksystem/BankApplication/BankApplication/model/TransactionRecord.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace BankApplication.model
+{
+	public class TransactionRecord
+	{
+        
+            public string FromAccountNumber { get; }
+            public string ToAccountNumber { get; }
+            public double Amount { get; }
+            public DateTime Timestamp { get; }
+
+            public TransactionRecord(string fromAccountNumber, string toAccountNumber, double amount, DateTime timestamp)
+            {
+                FromAccountNumber = fromAccountNumber;
+                ToAccountNumber = toAccountNumber;
+                Amount = amount;
+                Timestamp = timestamp;
+            }
+        
+    }
+}
+
